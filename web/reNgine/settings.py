6d73eadba8aafc63fe8e38588d43f0edfeda765e
Rohit Gurkhe <rohitgurkhe@gmail.com>
2023-10-20 13:47:15 +0530

    jul23

--- web/reNgine/settings.py
+++ web/reNgine/settings.py
@@ -77,6 +77,25 @@ TEMPLATES = [
 WSGI_APPLICATION = "reNgine.wsgi.application"
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "*",  # for network (Development)
+]
 # REST Framework
 
 REST_FRAMEWORK = {
