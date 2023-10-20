c6034bb43714bf0e6ac05f86fa37382c0311a94c
Rohit Gurkhe <rohitgurkhe@gmail.com>
2023-10-20 13:51:48 +0530

    jul23

--- web/reNgine/settings.py
+++ web/reNgine/settings.py
@@ -78,7 +78,10 @@ WSGI_APPLICATION = "reNgine.wsgi.application"
 
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ALLOWED_ORIGINS = ["*"]
-CORS_ORIGIN_WHITELIST = ("*",)
+# CORS_ORIGIN_WHITELIST = ()
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^*$",
+]
 CORS_ALLOW_HEADERS = [
     "accept",
     "accept-encoding",
