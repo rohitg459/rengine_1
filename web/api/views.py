5b09289af35528bbf0d6cd1fccf3599c0500d773
Rohit Gurkhe <rohitgurkhe@gmail.com>
2023-10-21 20:22:44 +0530

    jul23

--- web/api/views.py
+++ web/api/views.py
@@ -1869,7 +1869,7 @@ class IPToDomain(APIView):
     def get(self, request):
         req = self.request
         print(req.query_params, "q")
-        ip_address = req.query_params.get("ip_address")[0]
+        ip_address = req.query_params.get("ip_address")
         print(ip_address, "q")
         try:
             if ip_address:
