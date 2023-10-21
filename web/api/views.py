603ac78c6591d6d0e5d4fa9f4bcfc48259e3141a
Rohit Gurkhe <rohitgurkhe@gmail.com>
2023-10-21 20:24:54 +0530

    jul23

--- web/api/views.py
+++ web/api/views.py
@@ -1870,64 +1870,59 @@ class IPToDomain(APIView):
         req = self.request
         print(req.query_params, "q")
         ip_address = req.query_params.get("ip_address")
-        print(ip_address, "q")
-        try:
-            if ip_address:
-                options = FirefoxOptions()
-                options.add_argument("--headless")
-                driver = webdriver.Firefox(options=options)
+        if ip_address:
+            print(ip_address, "q")
+            options = FirefoxOptions()
+            options.add_argument("--headless")
+            driver = webdriver.Firefox(options=options)
 
-                # ip address may contain ip or CIDR, for ip use ip for CIDR use address
-                # as /net
-                if "/" in ip_address:
-                    driver.get("https://bgp.he.net/net/{}#_dns".format(ip_address))
-                else:
-                    driver.get("https://bgp.he.net/ip/{}#_dns".format(ip_address))
+            # ip address may contain ip or CIDR, for ip use ip for CIDR use address
+            # as /net
+            if "/" in ip_address:
+                driver.get("https://bgp.he.net/net/{}#_dns".format(ip_address))
+            else:
+                driver.get("https://bgp.he.net/ip/{}#_dns".format(ip_address))
 
-                try:
-                    element = WebDriverWait(driver, 30).until(
-                        EC.presence_of_element_located((By.ID, "tab_dns"))
-                    )
-                    # get all elements
-                    elems = driver.find_elements_by_xpath(
-                        "//a[starts-with(@href, '/dns/')]"
-                    )
-                    # remove empty domains as well
-                    domains = [elem.text for elem in elems if elem.text]
+            try:
+                element = WebDriverWait(driver, 30).until(
+                    EC.presence_of_element_located((By.ID, "tab_dns"))
+                )
+                # get all elements
+                elems = driver.find_elements_by_xpath(
+                    "//a[starts-with(@href, '/dns/')]"
+                )
+                # remove empty domains as well
+                domains = [elem.text for elem in elems if elem.text]
 
-                    # make domains list unique
-                    domains = list(set(domains))
+                # make domains list unique
+                domains = list(set(domains))
 
-                    response = {
-                        "status": True,
-                        "ip_address": ip_address,
-                        "domains": domains,
-                        "resolves_to": domains[0],
-                    }
+                response = {
+                    "status": True,
+                    "ip_address": ip_address,
+                    "domains": domains,
+                    "resolves_to": domains[0],
+                }
 
-                    # whois data
-                    # click on whois tab
-                    whois_button = driver.find_element_by_xpath("//li[@id='tab_whois']")
-                    whois_button.click()
+                # whois data
+                # click on whois tab
+                whois_button = driver.find_element_by_xpath("//li[@id='tab_whois']")
+                whois_button.click()
 
-                    whois_element = driver.find_element_by_xpath(
-                        "//div[@id='whois']/pre"
-                    )
-                    if whois_element:
-                        response["whois"] = whois_element.text
-                except Exception as e:
-                    print(e, "log")
-                    logging.error(e)
-                    response = {
-                        "status": False,
-                        "ip_address": ip_address,
-                        "message": "Exception {}".format(e),
-                    }
-                finally:
-                    driver.quit()
-                    return Response(response)
-        except Exception as e:
-            print(e, "log")
+                whois_element = driver.find_element_by_xpath("//div[@id='whois']/pre")
+                if whois_element:
+                    response["whois"] = whois_element.text
+            except Exception as e:
+                print(e, "log")
+                logging.error(e)
+                response = {
+                    "status": False,
+                    "ip_address": ip_address,
+                    "message": "Exception {}".format(e),
+                }
+            finally:
+                driver.quit()
+                return Response(response)
         return Response(
             {"status": False, "message": "IP Address Required", "q": req.query_params}
         )
